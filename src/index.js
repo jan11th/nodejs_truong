051aa2e7ddfed5cce286edd314f7b07d03063bec
truong2001 <hoangtruong11012001@gmail.com>
2023-02-11 03:18:20 +0700

    11-8

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+
 //http logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //template engine
 app.engine('hbs', engine({
@@ -23,7 +28,14 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
     res.render('news');
 });
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+app.post('/search', (req, res) => {
+  console.log(req.body)
+    res.send('');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 });
