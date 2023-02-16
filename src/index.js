efe3ada781ffe5b0a6bd642a3901aec32db96215
truong2001 <hoangtruong11012001@gmail.com>
2023-02-16 21:27:59 +0700

    prettier

--- src/index.js
+++ src/index.js
@@ -5,37 +5,33 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //http logger
 // app.use(morgan('combined'));
 
 //template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');  
-});
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-app.post('/search', (req, res) => {
-  console.log(req.body)
-    res.send('');
-});
+//routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`);
 });
